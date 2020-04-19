00c6e47c1bbcb33f9f3df38273a0799988adf74b
Natasha Wolfe <natasha.c.wolfe@gmail.com>
2020-04-18 17:42:15 -0700

    updated routes in server.js

--- server.js
+++ server.js
@@ -22,6 +22,8 @@ mongoose.connect("mongodb://localhost/budget", {
 
 // routes
 app.use(require("./routes/api.js"));
+app.use(require("./routes/htmlRoutes"));
+
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
