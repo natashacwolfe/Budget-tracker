9dbc4a3390b07dc61528f451ba43b8604ea95b07
Natasha Wolfe <natasha.c.wolfe@gmail.com>
2020-04-20 10:05:43 -0700

    update html routes for heroku

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,8 +2,9 @@ var path = require("path");
 const router = require("express").Router();
 
 
-module.exports = function() {
+
   router.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
-};
+
+  module.exports = router;
