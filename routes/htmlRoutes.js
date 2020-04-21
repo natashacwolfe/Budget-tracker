51209db99ee43e5620014ea88c7ec7539a8514f3
Natasha Wolfe <natasha.c.wolfe@gmail.com>
2020-04-21 16:49:35 -0700

    changed html route for heroku

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,8 +1,11 @@
 var path = require("path");
+const router = require("express").Router();
 
 
-module.exports = function(app) {
-  app.get("/", function(req, res) {
+
+
+  router.get("/", function(req, res) {
       res.sendFile(path.join(__dirname, "../public/index.html"));
   });
-};
+
+  module.exports = router
